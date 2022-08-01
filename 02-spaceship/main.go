e5ff6361483a580e460c4e1c7929f520ef85466d
Aliénor Latour <alienor.latour@hellofresh.com>
2022-08-01 10:58:18 +0200

    comments to save lives

--- 02-spaceship/main.go
+++ 02-spaceship/main.go
@@ -9,9 +9,12 @@ import (
 const blinkTime = time.Millisecond * 250
 
 func main() {
+
+	// set up the button
 	button := machine.D2
 	button.Configure(machine.PinConfig{Mode: machine.PinInput})
 
+	// set up the lights
 	red1 := machine.D5
 	red1.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	red2 := machine.D4
@@ -21,10 +24,12 @@ func main() {
 
 	for {
 		if button.Get() {
+			// if the button is pressed, turn on the green
 			green.Set(true)
 			red1.Set(false)
 			red2.Set(false)
 		} else {
+			// blink the red lights
 			green.Set(false)
 			red1.Set(true)
 			red2.Set(true)
